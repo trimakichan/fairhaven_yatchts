1218d31426608aaac6e013d9c8b5457699f043f8
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-17 19:37:53 -0700

    Update to serve from dist directory

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ app.use(cors());
 //     })
 // };
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/Frontend/build')));
+    app.use(express.static(path.join(__dirname, 'Frontend', 'dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'Frontend', 'dist', 'index.html'));
     });
 };
 
