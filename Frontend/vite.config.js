34b7ec15041dba55c6520ea3620d71479d327096
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-17 17:28:36 -0700

    Install @vitejs/plugin-react and update dependencies

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,31 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-    // server: {
-    //   // host: '0.0.0.0', // Bind to all network interfaces
-    //   // port: 5174,
-
-    //   // change here before production?
-    //   proxy: {
-    //     // Proxying API requests
-    //     '/api': {
-    //       target: 'https://api.boats.com', // Target API
-    //       changeOrigin: true,  // Needed for virtual hosted sites
-    //       rewrite: (path) => path.replace(/^\/api/, ''),  // Rewrite path removing '/api'
-    //     }
-    //   }
-    // },
-
-  // Configuration options for the production build
-  build: {
-    minify: 'esbuild', // Using esbuild for faster builds; switch to 'terser' for more complex scenarios
-    sourcemap: false, // Turning off source maps for production to reduce size and hide source
-  }
+  plugins: [react()]
 });
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+//     // server: {
+//     //   // host: '0.0.0.0', // Bind to all network interfaces
+//     //   // port: 5174,
+
+//     //   // change here before production?
+//     //   proxy: {
+//     //     // Proxying API requests
+//     //     '/api': {
+//     //       target: 'https://api.boats.com', // Target API
+//     //       changeOrigin: true,  // Needed for virtual hosted sites
+//     //       rewrite: (path) => path.replace(/^\/api/, ''),  // Rewrite path removing '/api'
+//     //     }
+//     //   }
+//     // },
+
+//   // Configuration options for the production build
+//   build: {
+//     minify: 'esbuild', // Using esbuild for faster builds; switch to 'terser' for more complex scenarios
+//     sourcemap: false, // Turning off source maps for production to reduce size and hide source
+//   }
+// });
 
 
 // import { defineConfig } from 'vite'
