5e7385674209598908cac1ad573effb1d9f08c55
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-06-17 21:46:31 -0700

    add Procfile

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000
 
 const app = express();
+//Enable cors
+app.use(cors());
+//Rate Limiting
+const limiter = rateLimit({
+    windowsMs: 10 * 60 * 1000, //10mins
+    max: 100
+});
+app.use(limiter)
+app.set('trust proxy', 1);
 
-// app.get("/*", function (req, res) {
-//     res.sendFile(__dirname, "./Frontend/dist/index.html"),
-//         function (err) {
-//             if (err) {
-//                 res.status(500).send(err);
-//             }
-//         }
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-// })
+
+
+
+//Routes
+app.use('/api', require('./routes'))
+
+
+// Serve static files from the Frontend/dist directory in production
 if (process.env.NODE_ENV === 'production') {
     // Serve static files from the Frontend/dist directory
     app.use(express.static(path.join(__dirname, 'Frontend', 'dist')));
@@ -31,43 +44,6 @@ if (process.env.NODE_ENV === 'production') {
         });
     });
 }
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-
-//Rate Limiting
-const limiter = rateLimit({
-    windowsMs: 10 * 60 * 1000, //10mins
-    max: 100
-})
-
-app.use(limiter)
-app.set('trust proxy', 1)
-
-
-//Routes
-app.use('/api', require('./routes'))
-
-//Enable cors
-app.use(cors());
-
-//this runs our app in heroku
-// const __dirname = path.resolve()
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '/Frontend/dist')))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'Frontend', 'dist', 'index.html'))
-//     })
-// };
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, 'Frontend', 'dist')));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'Frontend', 'dist', 'index.html'));
-//     });
-// };
-
 
 
 
