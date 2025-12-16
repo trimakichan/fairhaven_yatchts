d88fb871a683f9f81ca4ec4272db208ea0389af4
Makiko <trimakichan@gmail.com>
2025-12-15 21:01:28 -0800

    refactor: remove unused contact information section in TeamMember component

--- Frontend/src/components/TeamMember/TeamMember.jsx
+++ Frontend/src/components/TeamMember/TeamMember.jsx
@@ -1,9 +1,6 @@
 import "./teamMember.scss";
 import { Contexts } from "../../contexts/contexts";
-import { IoPhonePortrait } from "react-icons/io5";
-import { MdEmail } from "react-icons/md";
 import { useContext } from "react";
-
 
 const TeamMember = ({ memberInfo: m }) => {
 
@@ -25,41 +22,10 @@ const { setMemberMain } = useContext(Contexts);
             </div>
           </figcaption>
         </figure>
-
-        {/* <div className="teamMember-bottom">
-          <div className="textMRoboto">{m.description}</div>
-          <div className="teamMember-contactInfo">
-            {m.cell && (
-              <div className="icon-container">
-                <IoPhonePortrait
-                  className="iconStyles"
-                  aria-label="Phone Icon"
-                />
-                <a
-                  href={`tel:${m.cell}`}
-                  aria-label={`Call ${m.firstName} ${m.lastName} at +${m.cell}`}
-                >
-                  +{m.cell}
-                </a>
-              </div>
-            )}
-            {m.email && (
-              <div className="icon-container">
-                <MdEmail className="iconStyles" aria-label="Email Icon" />{" "}
-                <a
-                  href={`mailto:${m.email}`}
-                  aria-label={`Email ${m.firstName} ${m.lastName}`}
-                >
-                  {" "}
-                  {m.email}
-                </a>
-              </div>
-            )}
-          </div>
-        </div> */}
       </section>
     </a>
   );
+  
 };
 
 export default TeamMember;
